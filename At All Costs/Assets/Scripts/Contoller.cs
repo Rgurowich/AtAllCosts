d06957c816e423d2c09710a6fbba8ad713ff0c62
Rgurowich <ross.gurowich@students.plymouth.ac.uk>
2018-02-21 12:49:55 +0000

    Started 2d controller and dialogue system

---
+++ At All Costs/Assets/Scripts/Contoller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contoller : MonoBehaviour {
+
+    public float movementSpeed;
+    float valX;
+    float valY;
+    bool facingRight = true;
+    Rigidbody2D rigBody;
+
+	void Start () {
+        rigBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        valX = Input.GetAxisRaw("Horizontal");
+        valY = rigBody.velocity.y;
+        rigBody.velocity = new Vector2(valX * movementSpeed, valY);
+	}
+
+    void LateUpdate()
+    {
+        Vector3 localScale = transform.localScale;
+        if (valX > 0)
+        {
+            facingRight = true;
+        } else if (valX < 0)
+        {
+            facingRight = false;
+        }
+        if(((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0))){
+            localScale.x *= -1;
+        }
+
+        transform.localScale = localScale;
+    }
+}
---
+++ At All Costs/Assets/Scripts/dialogueManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogueManager : MonoBehaviour {
+
+    public Text NameText;
+    public Text DialogueText;
+
+    public Animator animator;
+
+    private Queue<string> speech;
+ 
+	void Start () {
+        speech = new Queue<string>();
+	}
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+
+        NameText.text = dialogue.name;
+
+        speech.Clear();
+
+        foreach (string sentance in dialogue.sentances)
+        {
+            speech.Enqueue(sentance);
+        }
+
+        DisplayNectSentence();
+    }
+
+    public void DisplayNectSentence()
+    {
+        if(speech.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentance = speech.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentance));
+    }
+
+    IEnumerator TypeSentence (string sentance)
+    {
+        DialogueText.text = "";
+        foreach (char letter in sentance.ToCharArray())
+        {
+            DialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+      
+
+
+
+}
