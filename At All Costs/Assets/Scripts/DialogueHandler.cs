c4d3bc1707501265929e12b7aeeba8b98f2f6481
Rgurowich <ross.gurowich@students.plymouth.ac.uk>
2018-03-21 10:01:46 +0000

    Art updated and minigame started

--- At All Costs/Assets/Scripts/DialogueHandler.cs
+++ At All Costs/Assets/Scripts/DialogueHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class DialogueHandler : MonoBehaviour {
 
@@ -12,10 +13,10 @@ public class DialogueHandler : MonoBehaviour {
     public Text nameText;
     public Text messageText;
 
-    public Button reponseOne;
-    public Button reponseTwo;
-    public Button reponseThree;
-    public Button reponseFour;
+    public GameObject reponseOne;
+    public GameObject reponseTwo;
+    public GameObject reponseThree;
+    public GameObject reponseFour;
     public Text reponseTextOne;
     public Text reponseTextTwo;
     public Text reponseTextThree;
@@ -27,16 +28,12 @@ public class DialogueHandler : MonoBehaviour {
 
     private bool entered = false;
 
-    void Start()
-    {
-        LoadDialogue();
-    }
-
     void Update()
     {
-        if (Input.GetKey(KeyCode.E) && entered == true)
+        if (Input.GetKeyDown(KeyCode.E) && entered == true)
         {
             dialogueBox.SetActive(true);
+            LoadDialogue();
         }
     }
 
@@ -60,29 +57,89 @@ public class DialogueHandler : MonoBehaviour {
     {
         currentIndex = 0;
         nameText.text = dialogue.npcName;
+        Debug.Log(dialogue.npcName);
         messageText.text = dialogue.messages[0].text;
+        Debug.Log(dialogue.messages[0].text);
 
         reponseTextOne.text = dialogue.messages[0].responses[0].reply;
-        reponseTextTwo.text = dialogue.messages[0].responses[1].reply;
-        reponseTextThree.text = dialogue.messages[0].responses[2].reply;
+        //reponseTextTwo.text = dialogue.messages[0].responses[1].reply;
+        //reponseTextThree.text = dialogue.messages[0].responses[2].reply;
         //reponseTextFour.text = dialogue.messages[0].responses[3].reply;
     }
 
+ 
     public void LoadText()
     {
+        int buttonPressed = 0;
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
         
-        string currentMsg = dialogue.messages[msgNum].text;
-        Debug.Log(currentMsg);
-        //int currentMsgIndex = Array.IndexOf(dialogue.messages, currentMsg) + 1;
-        //Debug.Log(currentMsgIndex);
-        int nextMsg = dialogue.messages[currentIndex].responses[0].next;
-        Debug.Log(nextMsg);
+        if (buttonName == "ResponseOne")
+        {
+            buttonPressed = 0;
+        }
+        if (buttonName == "ResponseTwo")
+        {
+            buttonPressed = 1;
+        }
+        if (buttonName == "ResponseThree")
+        {
+            buttonPressed = 2;
+        }
+        if (buttonName == "ResponseFour")
+        {
+            buttonPressed = 3;
+        }
 
+        reponseOne.SetActive(false);
+        reponseTwo.SetActive(false);
+        reponseThree.SetActive(false);
+        reponseFour.SetActive(false);
+
+        int nextMsg = dialogue.messages[currentIndex].responses[buttonPressed].next;
+        int resLength = dialogue.messages[nextMsg].responses.Length;
+        Debug.Log("Res Length = " + resLength);
+        Debug.Log("Next Message = " + nextMsg);
         messageText.text = dialogue.messages[nextMsg].text;
-        reponseTextOne.text = dialogue.messages[nextMsg].responses[0].reply;
+        if(resLength == 1)
+        {
+            reponseOne.SetActive(true);
+            reponseTextOne.text = dialogue.messages[nextMsg].responses[0].reply;
+        }
+        if (resLength == 2)
+        {
+            reponseOne.SetActive(true);
+            reponseTextOne.text = dialogue.messages[nextMsg].responses[0].reply;
+            reponseTwo.SetActive(true);
+            reponseTextTwo.text = dialogue.messages[nextMsg].responses[1].reply;
+        }
+        if (resLength == 3)
+        {
+            reponseOne.SetActive(true);
+            reponseTextOne.text = dialogue.messages[nextMsg].responses[0].reply;
+            reponseTwo.SetActive(true);
+            reponseTextTwo.text = dialogue.messages[nextMsg].responses[1].reply;
+            reponseThree.SetActive(true);
+            reponseTextThree.text = dialogue.messages[nextMsg].responses[2].reply;
+
+        }
+        if (resLength == 4)
+        {
+            reponseOne.SetActive(true);
+            reponseTextOne.text = dialogue.messages[nextMsg].responses[0].reply;
+            reponseTwo.SetActive(true);
+            reponseTextTwo.text = dialogue.messages[nextMsg].responses[1].reply;
+            reponseThree.SetActive(true);
+            reponseTextThree.text = dialogue.messages[nextMsg].responses[2].reply;
+            reponseFour.SetActive(true);
+            reponseTextFour.text = dialogue.messages[nextMsg].responses[3].reply;
+
+        }
         msgNum = nextMsg;
         Debug.Log(msgNum);
-
         currentIndex++;
+        //if (currentIndex >= dialogue.messages.Length)
+        //{
+        //    currentIndex -= 3;
+        //}
     }
 }
---
+++ At All Costs/Assets/Scripts/MiniGames/WireMiniGame.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class WireMiniGame : MonoBehaviour {
+
+    public double timeLeft;
+    public Text timeDisplay;
+    public string FirstWireName;
+    public Button[] Wires;
+    public Image[] PowerBars;
+
+    private bool blueConnected;
+    private bool redConnected;
+    private bool purpleConnected;
+    private bool greenConnected;
+
+    // Use this for initialization
+    void Start () {
+        FirstWireName = "Not Selected";
+        greenConnected = false;
+        blueConnected = false;
+        redConnected = false;
+        purpleConnected = false;
+}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        timeDisplay.text = timeLeft.ToString("00:00");
+        if(FirstWireName == "Not Selected")
+        {
+            Wires[0].interactable = true;
+            Wires[1].interactable = true;
+            Wires[2].interactable = true;
+            Wires[3].interactable = true;
+            Wires[4].interactable = true;
+            Wires[5].interactable = true;
+            Wires[6].interactable = true;
+            Wires[7].interactable = true;
+        }
+
+        if(greenConnected == true && purpleConnected == true && blueConnected == true && redConnected == true)
+        {
+            timeDisplay.text = "YAAAY";
+            Wires[0].interactable = false;
+            Wires[1].interactable = false;
+            Wires[2].interactable = false;
+            Wires[3].interactable = false;
+            Wires[4].interactable = false;
+            Wires[5].interactable = false;
+            Wires[6].interactable = false;
+            Wires[7].interactable = false;
+        }
+    }
+
+    public void GetFirstWire()
+    {
+        if (FirstWireName == "Not Selected")
+        {
+            FirstWireName = EventSystem.current.currentSelectedGameObject.name;
+            Debug.Log(FirstWireName);
+            if (FirstWireName == "TopBlue" || FirstWireName == "TopRed" || FirstWireName == "TopPurple" || FirstWireName == "TopGreen")
+            {
+                Wires[0].interactable = false;
+                Wires[1].interactable = false;
+                Wires[2].interactable = false;
+                Wires[3].interactable = false;
+                Wires[4].interactable = true;
+                Wires[5].interactable = true;
+                Wires[6].interactable = true;
+                Wires[7].interactable = true;
+                
+        }
+            if (FirstWireName == "BottomBlue" || FirstWireName == "BottomRed" || FirstWireName == "BottomPurple" || FirstWireName == "BottomGreen")
+            {
+                Wires[0].interactable = true;
+                Wires[1].interactable = true;
+                Wires[2].interactable = true;
+                Wires[3].interactable = true;
+                Wires[4].interactable = false;
+                Wires[5].interactable = false;
+                Wires[6].interactable = false;
+                Wires[7].interactable = false;
+            }
+        }
+        else
+        {
+            string secondWireName = EventSystem.current.currentSelectedGameObject.name;
+            if (FirstWireName == "TopBlue" && secondWireName == "BottomBlue" || FirstWireName == "BottomBlue" && secondWireName == "TopBlue")
+            {
+                Debug.Log("BLUE AND BLUE");
+                FirstWireName = "Not Selected";
+                blueConnected = true;                
+            }         
+            else if (FirstWireName == "TopRed" && secondWireName == "BottomRed" || FirstWireName == "BottomRed" && secondWireName == "TopRed")
+            {
+                Debug.Log("RED AND RED");
+                FirstWireName = "Not Selected";
+                redConnected = true;
+            }
+            else if (FirstWireName == "TopPurple" && secondWireName == "BottomPurple" || FirstWireName == "BottomPurple" && secondWireName == "TopPurple")
+            {
+                Debug.Log("PURPLE AND PURPLE");
+                FirstWireName = "Not Selected";
+                purpleConnected = true;
+            }
+            else if (FirstWireName == "TopGreen" && secondWireName == "BottomGreen" || FirstWireName == "BottomGreen" && secondWireName == "TopGreen")
+            {
+                Debug.Log("GREEN AND GREEN");
+                FirstWireName = "Not Selected";
+                greenConnected = true;
+            }
+            else
+            {
+                Debug.Log("WRONG");
+                FirstWireName = "Not Selected";
+            }
+        }
+        
+
+    }
+}
